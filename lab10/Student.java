70d980dd1e67da2d3dea668665e19736f0018c18
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-10-27 18:51:03 +0530

    lab10 adds files

---
+++ lab10/Student.java
@@ -0,0 +1,26 @@
+/**
+ * Created by saikumar on 27/10/16 at 2:17 PM.
+ */
+public class Student {
+    private int id;
+    private String  department;
+    private int marks;
+
+    public Student(int id, String department, int marks) {
+        this.id = id;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+}
